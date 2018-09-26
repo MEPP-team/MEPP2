3d72ac8a4614ef858bf31a87b2f3e8c33a00e700
Vincent Vidal <vincent.vidal@liris.cnrs.fr>
2018-09-26 16:07:59 +0200

    Minor modifications

--- Testing/Utils/utils_retrieve_vertex.h
+++ Testing/Utils/utils_retrieve_vertex.h
@@ -7,21 +7,22 @@
  * \tparam MutableFaceGraph Any mesh type having a boost::graph_traits
  *                    specialisation.
  * \param g           The graph which which to search for.
- * \param targetIndex The index of the target vertex.
+ * \param target_index The index of the target vertex.
  * \return            When found, the vertex with target index.
  */   
 template<typename MutableFaceGraph>
 typename boost::graph_traits<MutableFaceGraph>::vertex_descriptor
 retrieve_vertex( MutableFaceGraph& g,
-                 int targetIndex )
+                 int target_index )
 {
   typedef boost::graph_traits<MutableFaceGraph>     GraphTraits;
   typedef typename GraphTraits::vertex_iterator     vertex_iterator;
   typedef typename GraphTraits::vertex_descriptor   vertex_descriptor;
 
-  vertex_iterator v = vertices(g).first; // g.vertices_begin() cannot be used here, because it is not defined in any concept
+  vertex_iterator v = vertices(g).first; // g.vertices_begin() cannot be used here, 
+                                         // because it is not defined in any concept
 
-  vertex_descriptor dst = *boost::next(v, targetIndex);
+  vertex_descriptor dst = *boost::next(v, target_index);
 
   return dst;
 }
