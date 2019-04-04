5f3c74fa6c3517498839fdf72e57bf80567be5c1
Martial Tola <martial.tola@liris.cnrs.fr>
2019-04-04 19:39:10 +0200

    update cmake file for GMP and MPFR libraries

--- Cmake/msvc/kit_msvc-14.x-64.cmake
+++ Cmake/msvc/kit_msvc-14.x-64.cmake
@@ -17,6 +17,14 @@ else()
 endif()
 
 set(CGAL_DIR					${MSVC_KIT_ROOT}/CGAL-4.11-bug-patched)
+# -------
+# for GMP
+set(GMP_INCLUDE_DIR             ${MSVC_KIT_ROOT}/gmp/include)
+set(GMP_LIBRARIES               ${MSVC_KIT_ROOT}/gmp/lib/libgmp-10.lib)
+# for MPFR
+set(MPFR_INCLUDE_DIR            ${MSVC_KIT_ROOT}/gmp/include)
+set(MPFR_LIBRARIES              ${MSVC_KIT_ROOT}/gmp/lib/libmpfr-4.lib)
+# -------
 
 set(OPENMESH_DIR				${MSVC_KIT_ROOT}/OpenMesh-6.2)
 
