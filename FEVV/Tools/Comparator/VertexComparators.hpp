daa24e48b95ccfa3cbc46cc417b03d80e8531d3b
Vincent Vidal <vincent.vidal@liris.cnrs.fr>
2019-05-15 23:08:50 +0200

    VertexComparators.hpp: remove an author comment and then add more conditions in the vertex comparator to get less equality cases. This is useful for spanning tree generation, as equality cases may cause a different spanning tree generation.

--- FEVV/Tools/Comparator/VertexComparators.hpp
+++ FEVV/Tools/Comparator/VertexComparators.hpp
@@ -8,15 +8,10 @@
 //
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-/**
- * \file		helpers.hxx
- * \author		Vincent Vidal
- * \date     	2017-12-01
- */
-
 #pragma once
 
 #include <limits>
+#include <iterator>
 
 namespace FEVV { 
 namespace Comparator 
@@ -31,11 +26,12 @@ namespace Comparator
     typedef typename Geometry::Scalar                         Scalar;
     typedef typename Geometry::Point                          Point;
   private:
+    const Graph& _g;
     const PointMap& _pm;
     const GeometryTraits _gt;
   public:
-    VertexComparator(const Graph& g, const PointMap& pm) :_pm(pm), _gt(GeometryTraits(g)) {}
-    VertexComparator(const PointMap& pm, const GeometryTraits& gt) :_pm(pm), _gt(gt) {}
+    VertexComparator(const Graph& g, const PointMap& pm) :_g(g), _pm(pm), _gt(GeometryTraits(g)) {}
+    VertexComparator(const Graph& g, const PointMap& pm, const GeometryTraits& gt) :_g(g), _pm(pm), _gt(gt) {}
     bool operator()(vertex_descriptor v1, vertex_descriptor v2)
     {
       Point pv1 = get(_pm, v1);
@@ -45,8 +41,36 @@ namespace Comparator
         return _gt.get_x(pv1) < _gt.get_x(pv2);
       else if(fabs(_gt.get_y(pv1) - _gt.get_y(pv2)) > std::numeric_limits<Scalar>::epsilon())
         return _gt.get_y(pv1) < _gt.get_y(pv2);
-      else
+      else if(fabs(_gt.get_z(pv1) - _gt.get_z(pv2)) > std::numeric_limits<Scalar>::epsilon())
         return _gt.get_z(pv1) < _gt.get_z(pv2);
+	
+      auto e_v1 = in_edges(v1,_g), 
+           e_v2 = in_edges(v2,_g);
+	     
+      auto e_v1_deg = std::distance(e_v1.first, e_v1.second),
+           e_v2_deg = std::distance(e_v2.first, e_v2.second);
+      if( e_v1_deg != e_v2_deg)
+		  return e_v1_deg < e_v2_deg ;
+	  
+      e_v1_deg=e_v2_deg=0;
+      auto iter_e_v = e_v1.first;
+	  for( ; iter_e_v!=e_v1.second; ++iter_e_v)
+	  {
+		  auto e_vopp = in_edges(source(*iter_e_v, _g),_g);
+		  e_v1_deg += std::distance(e_vopp.first, e_vopp.second);
+	  }
+	  
+	  iter_e_v = e_v2.first;
+	  for( ; iter_e_v!=e_v2.second; ++iter_e_v)
+	  {
+		  auto e_vopp = in_edges(source(*iter_e_v, _g),_g);
+		  e_v2_deg += std::distance(e_vopp.first, e_vopp.second);
+	  }
+	  
+	  if( e_v1_deg != e_v2_deg)
+        return e_v1_deg < e_v2_deg ;
+	  
+	  return false;
     }
   };
   /////////////////////////////////////////////////////////////////////////////
