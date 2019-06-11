0984d7db870b62c6598e2b5246cf3e6443da333c
Martial Tola <martial.tola@liris.cnrs.fr>
2019-06-11 15:23:44 +0200

    fix new cgal macro redefinitions with cgal 4.14

--- FEVV/DataStructures/DataStructures_cgal_linear_cell_complex.h
+++ FEVV/DataStructures/DataStructures_cgal_linear_cell_complex.h
@@ -10,21 +10,7 @@
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 #pragma once
 
-// IMPORTANT !!!
-// NOTE : if we include <CGAL/version_macros.h> we have macro redefinition of
-// CGAL_VERSION_STR
-#ifndef CGAL_VERSION_MAJOR
-#define CGAL_VERSION_MAJOR (CGAL_VERSION_NR / 10000000 % 100)
-#endif
-
-#ifndef CGAL_VERSION_MINOR
-#define CGAL_VERSION_MINOR (CGAL_VERSION_NR / 100000 % 100)
-#endif
-
-#ifndef CGAL_VERSION_PATCH
-#define CGAL_VERSION_PATCH (CGAL_VERSION_NR / 10000 % 10)
-#endif
-// IMPORTANT !!!
+#include <CGAL/version_macros.h>
 
 #include <CGAL/Cartesian.h>
 
