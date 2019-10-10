8394a9613e8908b7f9c41c461f35cc130797f58c
elombardi2 <eric.lombardi@liris.cnrs.fr>
2019-10-10 09:10:37 +0200

    Change CGALPointSet kernel to float and color to uchar

--- FEVV/DataStructures/DataStructures_cgal_point_set.h
+++ FEVV/DataStructures/DataStructures_cgal_point_set.h
@@ -13,14 +13,53 @@
 #include <CGAL/version_macros.h>
 #include <CGAL/Cartesian.h>
 #include <CGAL/Point_set_3.h>
+#include <CGAL/IO/Color.h> // for std::array
 
 
 namespace FEVV {
 
 
-using CGALPointSetKernel = CGAL::Cartesian< double >;
+using CGALPointSetKernel = CGAL::Cartesian< float >;
 using CGALPointSetPoint  = CGALPointSetKernel::Point_3;
 using CGALPointSetVector = CGALPointSetKernel::Vector_3;
+
+// note:
+// - CGAL::Color in CGAL 4.14 misses operator[] that is used in MEPP2
+//   so we define our own color type derived from CGAL::Color
+// - starting from CGAL-5.0-beta1, CGAL::Color has everything needed
+//   by MEPP2 and we must then use it directly
+//TODO-elo-CGAL-5.0-beta1  using CGALPointSetColor = CGAL::Color;
+class CGALPointSetColor : public CGAL::Color
+{
+public:
+  // must redefine default constructor because
+  // constructor(r,g,b) is re-defined
+  CGALPointSetColor(void) : CGAL::Color()
+  {}
+
+  // must redefine constructor(r,g,b) to use
+  // base class constructor(r,g,b)
+  CGALPointSetColor(unsigned char r, unsigned char g, unsigned char b)
+      : CGAL::Color(r, g, b)
+  {}
+
+  // color must behave as a vector
+  unsigned char operator[](int i)	const
+  {
+    if(i == 0)
+      return r();
+    else if(i == 1)
+      return g();
+    else if(i == 2)
+      return b();
+    else
+    {
+      throw std::runtime_error(
+          "FEVV::CGALPointSetColor: invalid index in operator[]");
+      return 0; // dummy return to avoid a warning
+    }
+  }
+};
 
 using CGALPointSet = CGAL::Point_set_3< CGALPointSetPoint >;
 
