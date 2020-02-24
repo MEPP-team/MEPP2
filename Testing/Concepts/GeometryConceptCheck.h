06e59b02b31c5b20ec5f85f9e221f020d7579a7a
elombardi2 <eric.lombardi@liris.cnrs.fr>
2020-02-24 13:36:21 +0100

    Add checking of operators results in Geometry Concept checking

--- Testing/Concepts/GeometryConceptCheck.h
+++ Testing/Concepts/GeometryConceptCheck.h
@@ -10,6 +10,7 @@
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 #pragma once
 
+#include <cassert>
 #include <boost/concept_check.hpp>
 #include "FEVV/Wrappings/Geometry_traits.h"
 #include "FEVV/Wrappings/Geometry_traits_operators.h"
@@ -129,6 +130,105 @@ struct GeometryConcept
 #endif
   }
 };
+
+
+/**
+ * Check operators results.
+ */
+template< typename GeometryTraits >
+void check_operators_results(void)
+{
+  typedef typename GeometryTraits::Point  Point;
+  typedef typename GeometryTraits::Vector Vector;
+  typedef typename GeometryTraits::Scalar Scalar;
+
+  // helper lambda function to test if two FP numbers are almost equal
+  auto almost_equal_s = [](Scalar s1, Scalar s2, Scalar epsilon) -> bool
+  {
+    return std::abs(s1 - s2) <= epsilon;
+  };
+
+  // helper lambda function to test if two FP points are almost equal
+  auto almost_equal_p = [almost_equal_s](Point p1, Point p2, Scalar epsilon) -> bool
+  {
+    return almost_equal_s(GeometryTraits::get_x(p1),
+                          GeometryTraits::get_x(p2),
+                          epsilon) &&
+           almost_equal_s(GeometryTraits::get_y(p1),
+                          GeometryTraits::get_y(p2),
+                          epsilon) &&
+           almost_equal_s(GeometryTraits::get_z(p1),
+                          GeometryTraits::get_z(p2),
+                          epsilon);
+  };
+
+  // helper lambda function to test if two FP vectors are almost equal
+  auto almost_equal_v = [almost_equal_s](Vector v1, Vector v2, Scalar epsilon) -> bool
+  {
+    return almost_equal_s(v1[0], v2[0], epsilon) &&
+           almost_equal_s(v1[1], v2[1], epsilon) &&
+           almost_equal_s(v1[2], v2[2], epsilon);
+  };
+
+  Scalar eps = 1e-5;
+  Vector u(8.3, -3.2, -3.8);
+  Vector v(1.2, 2.3, 3.4);
+  Point p(3.0, 12.5, -2.1);
+  Point q(-1.4, 4.6, -1.9);
+  Point r(-2.6, 4.7, 5.8);
+
+  {
+    Scalar l2 = GeometryTraits::length2(v);
+    assert(almost_equal_s(l2, 18.29, eps));
+
+    Scalar l = GeometryTraits::length(v);
+    assert(almost_equal_s(l, 4.276681, eps));
+
+    Vector n = GeometryTraits::normalize(v);
+    assert(almost_equal_v(n, Vector(0.280591, 0.537800, 0.795009), eps));
+  }
+
+  {
+    Vector w = GeometryTraits::sub(p, q);
+    assert(almost_equal_v(w, Vector(4.4, 7.9, -0.2), eps));
+
+    Scalar l = GeometryTraits::length(p, q);
+    assert(almost_equal_s(l, 9.044888, eps));
+  }
+
+  {
+    Vector n = GeometryTraits::normal(p, q, r);
+    assert(almost_equal_v(n, Vector(-60.85, 33.64, -9.92), eps));
+    
+    n = GeometryTraits::unit_normal(p, q, r);
+    assert(almost_equal_v(n, Vector(-0.866393, 0.478972, -0.141243), eps));
+  }
+
+  {
+    Vector w = GeometryTraits::add(u, v);
+    assert(almost_equal_v(w, Vector(9.5, -0.9, -0.4), eps));
+
+    Scalar d = GeometryTraits::dot_product(u, v);
+    assert(almost_equal_s(d, -10.32, eps));
+
+    w = GeometryTraits::cross_product(u, v);
+    assert(almost_equal_v(w, Vector(-2.14, -32.78, 22.93), eps));
+  }
+
+  {
+    Point a = GeometryTraits::add_p(p, v);
+    assert(almost_equal_p(a, Point(4.2, 14.8, 1.3), eps));
+
+    a = GeometryTraits::sub_p(p, v);
+    assert(almost_equal_p(a, Point(1.8, 10.2, -5.5), eps));
+
+    Vector w = GeometryTraits::scalar_mult(v, -2.345);
+    assert(almost_equal_v(w, Vector(-2.814, -5.3935, -7.973), eps));
+
+    w = GeometryTraits::scalar_mult(-2.345, v);
+    assert(almost_equal_v(w, Vector(-2.814, -5.3935, -7.973), eps));
+  }
+}
 
 } // namespace FEVV
 
