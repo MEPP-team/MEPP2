b88bbbd6d41bc318ac3aa4eeb85b51637595cfeb
elombardi2 <eric.lombardi@liris.cnrs.fr>
2020-04-16 10:03:42 +0200

    Implement copy_graph plugin

---
+++ Visualization/PluginFilters/CopyGraph/copy_graph.cmake
@@ -0,0 +1,38 @@
+# --> CopyGraphPlugin : QtPlugin [BEGIN]
+OPTION( BUILD_USE_GUI_CopyGraphPlugin "BUILD CopyGraphPlugin " ON )
+if (BUILD_USE_GUI_CopyGraphPlugin)
+  set( CopyGraph_Qt_Plugin_HEADER
+       "${CMAKE_CURRENT_LIST_DIR}/copy_graph_plugin.h"
+       "${CMAKE_CURRENT_LIST_DIR}/Dialogs/copy_graph_dialog.h"
+       )
+  set( CopyGraph_Qt_Plugin_UI
+       "${CMAKE_CURRENT_LIST_DIR}/Dialogs/copy_graph_dialog.ui"
+       )
+  if (BUILD_USE_QT5)
+    QT5_WRAP_CPP(CopyGraph_Qt_Plugin_MOC_CPP ${CopyGraph_Qt_Plugin_HEADER})
+    QT5_WRAP_UI(CopyGraph_Qt_Plugin_UI_CPP ${CopyGraph_Qt_Plugin_UI})
+    set(CopyGraph_Qt_Plugin_SRC ${CopyGraph_Qt_Plugin_SRC} ${CopyGraph_Qt_Plugin_MOC_CPP} ${CopyGraph_Qt_Plugin_UI_CPP})
+  else(BUILD_USE_QT5)
+    QT4_WRAP_CPP(CopyGraph_Qt_Plugin_MOC_CPP ${CopyGraph_Qt_Plugin_HEADER})
+    QT4_WRAP_UI(CopyGraph_Qt_Plugin_UI_CPP ${CopyGraph_Qt_Plugin_UI})
+    set(CopyGraph_Qt_Plugin_SRC ${CopyGraph_Qt_Plugin_SRC} ${CopyGraph_Qt_Plugin_MOC_CPP} ${CopyGraph_Qt_Plugin_UI_CPP})
+  endif(BUILD_USE_QT5)
+
+  add_library(
+      CopyGraphPlugin
+      SHARED
+      "${CMAKE_CURRENT_LIST_DIR}/copy_graph_plugin.cpp"
+      "${CMAKE_CURRENT_LIST_DIR}/Dialogs/copy_graph_dialog.cpp"
+      ${CopyGraph_Qt_Plugin_SRC}
+      ${osgQt_SRC} # from viewer
+      )
+  target_link_libraries (CopyGraphPlugin ${CopyGraph_Qt_Plugin_LIB}
+    ${GUILIB_DEMO} # from viewer
+    )
+  add_dependencies(CopyGraphPlugin mepp-gui)
+endif (BUILD_USE_GUI_CopyGraphPlugin)
+# --> CopyGraphPlugin : QtPlugin [END]
+
+# remove the lines below to integrate into MEPP2-public
+#TODO-elo why is this line needed ?
+include_directories( "${CMAKE_CURRENT_BINARY_DIR}" )
---
+++ Visualization/PluginFilters/CopyGraph/copy_graph_plugin.h
@@ -0,0 +1,281 @@
+// Copyright (c) 2012-2019 University of Lyon and CNRS (France).
+// All rights reserved.
+//
+// This file is part of MEPP2; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation; either version 3 of the License, 
+// or (at your option) any later version.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#pragma once
+
+#if(_MSC_VER >= 1400)
+#ifndef _SCL_SECURE_NO_WARNINGS
+#define _SCL_SECURE_NO_WARNINGS
+#endif
+#endif
+
+#include "Visualization/Plugins/PluginInterface.h"
+
+#include <QStringList>
+#include "Dialogs/copy_graph_dialog.h"
+
+#ifndef Q_MOC_RUN // MT : very important to avoid the error : ' Parse error at
+                  // "BOOST_JOIN" ' -> (qt4 pb with boost)
+#include "Visualization/PluginFilters/BasePluginQt.h"
+#include "Visualization/SimpleViewer.h"
+
+#include "Visualization/SimpleWindow.h"
+
+// A) include the header of the filter corresponding to your operation
+#include "FEVV/Filters/Generic/copy_graph.hpp"
+
+#include "FEVV/Wrappings/properties.h"
+
+#ifdef FEVV_USE_CGAL
+#include "FEVV/Wrappings/properties_polyhedron_3.h"
+#include "FEVV/Wrappings/properties_surface_mesh.h"
+#include "FEVV/Wrappings/properties_linear_cell_complex.h"
+#endif // FEVV_USE_CGAL
+#ifdef FEVV_USE_OPENMESH
+#include "FEVV/Wrappings/properties_openmesh.h"
+#endif // FEVV_USE_OPENMESH
+#ifdef FEVV_USE_AIF
+#include "FEVV/Wrappings/properties_aif.h"
+#endif // FEVV_USE_AIF
+#endif
+
+namespace FEVV {
+
+class CopyGraphPlugin : public QObject,
+                         public Generic_PluginInterface,
+                         public BasePluginQt
+{
+  Q_OBJECT
+  Q_INTERFACES(FEVV::Generic_PluginInterface)
+#if(FEVV_USE_QT5) // see at the end of .cpp for QT4
+  Q_PLUGIN_METADATA(IID "CopyGraphPlugin")
+#endif
+
+  /*public:
+      using BasePlugin::apply;*/
+public:
+  CopyGraphPlugin() = default;
+  ~CopyGraphPlugin() = default;
+
+public:
+  void init() override
+  {
+    m_output_mesh_void = nullptr;
+    m_output_pmaps_bag = nullptr;
+  }
+
+  void reset() override
+  {
+    init();
+
+    emit resetSignal();
+  }
+
+  void addParameters(BaseWindow *_window) override
+  {
+    window = _window;
+    if(window == nullptr || !window->isInit())
+    {
+      std::cerr << "BaseWindow is null or not initialized." << std::endl;
+      return;
+    }
+  }
+
+  template< typename HalfedgeGraphS,
+            typename HalfedgeGraphT >
+  void process(const HalfedgeGraphS       *_mesh,
+               const FEVV::PMapsContainer *pmaps_bag)
+  {
+    std::cout << "Asking to CopyGraph mesh ! " << std::endl;
+
+    // create output mesh and pmaps bag
+    HalfedgeGraphT *output_mesh = new HalfedgeGraphT;
+    m_output_pmaps_bag = new FEVV::PMapsContainer;
+
+    // store output mesh for later display
+    m_output_mesh_void = static_cast< void * >(output_mesh);
+
+    // apply filter
+    FEVV::Filters::copy_graph(
+        *_mesh, *pmaps_bag, *output_mesh, *m_output_pmaps_bag);
+  }
+
+
+  template< typename HalfedgeGraphS,
+            typename HalfedgeGraphT >
+  void applyHG(BaseAdapterVisu      *_adapter,
+               HalfedgeGraphS       *_mesh,
+               FEVV::PMapsContainer *pmaps_bag)
+  {
+    // note: disable dialog box because no need for now
+    //// get filter parameters from dialog window
+    //CopyGraphDialog dialog;
+    //dialog.setParameters(value_x, value_y, value_z);
+    //if(dialog.exec() == QDialog::Accepted)
+    //  dialog.getParameters(value_x, value_y, value_z);
+    //else
+    //  return; // abort applying filter
+
+    // apply filter
+    process< HalfedgeGraphS, HalfedgeGraphT >(_mesh, pmaps_bag);
+
+    // redraw mesh
+    SimpleViewer *viewer =
+        dynamic_cast< SimpleViewer * >(_adapter->getViewer());
+    if(viewer)
+    {
+      // draw output mesh
+      auto output_mesh = static_cast< HalfedgeGraphT * >(m_output_mesh_void);
+      if(output_mesh)
+      {
+        viewer->draw_or_redraw_mesh(output_mesh,
+                                    m_output_pmaps_bag,
+                                    false,
+                                    false,
+                                    "COPY");
+      }
+    }
+
+    // comment next line to keep parameters values between calls
+    reset();
+
+    viewer->frame();
+  }
+
+
+#ifdef FEVV_USE_OPENMESH
+  void apply(BaseAdapterVisu *_adapter,
+             MeshOpenMesh *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    apply_choose_output< MeshOpenMesh >(_adapter, _mesh, pmaps_bag);
+  }
+#endif
+
+#ifdef FEVV_USE_CGAL
+  void apply(BaseAdapterVisu *_adapter,
+             MeshLCC *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    apply_choose_output< MeshLCC >(_adapter, _mesh, pmaps_bag);
+  }
+
+  void apply(BaseAdapterVisu *_adapter,
+             MeshSurface *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    apply_choose_output< MeshSurface >(_adapter, _mesh, pmaps_bag);
+  }
+
+  void apply(BaseAdapterVisu *_adapter,
+             MeshPolyhedron *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    apply_choose_output< MeshPolyhedron >(_adapter, _mesh, pmaps_bag);
+  }
+#endif
+
+#ifdef FEVV_USE_AIF
+  void apply(BaseAdapterVisu *_adapter,
+             MeshAIF *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    apply_choose_output< MeshAIF >(_adapter, _mesh, pmaps_bag);
+  }
+#endif
+
+
+  template< typename MeshT >
+  void apply_choose_output(BaseAdapterVisu      *_adapter,
+                           MeshT                *_mesh,
+                           FEVV::PMapsContainer *pmaps_bag)
+  {
+    // ask the user for the datastructure
+    std::string mesh_type = chooseDatastructureMsgBox();
+    if(mesh_type == "NONE")
+      return; // cancel pressed, aborting
+
+    // apply plugin
+    if(false)
+    {
+      // dummy case to be able to write all other cases as 'else if'
+    }
+#ifdef FEVV_USE_CGAL
+    else if(mesh_type == "POLYHEDRON")
+    {
+      applyHG< MeshT, MeshPolyhedron >(_adapter, _mesh, pmaps_bag);
+    }
+    else if(mesh_type == "SURFACEMESH")
+    {
+      applyHG< MeshT, MeshSurface >(_adapter, _mesh, pmaps_bag);
+    }
+    else if(mesh_type == "LCC")
+    {
+      applyHG< MeshT, MeshLCC >(_adapter, _mesh, pmaps_bag);
+    }
+#endif
+#ifdef FEVV_USE_OPENMESH
+    else if(mesh_type == "OPENMESH")
+    {
+      applyHG< MeshT, MeshOpenMesh >(_adapter, _mesh, pmaps_bag);
+    }
+#endif
+#ifdef FEVV_USE_AIF
+    else if(mesh_type == "AIF")
+    {
+      applyHG< MeshT, MeshAIF >(_adapter, _mesh, pmaps_bag);
+    }
+#endif
+#ifdef FEVV_USE_PCL
+    else
+    {
+      QMessageBox::information(0,
+                               "",
+                               QObject::tr("Copy Graph is not yet compatible "
+                                           "with the datastructure you have "
+                                           "chosen!"));
+    }
+#endif
+  }
+
+
+  QStringList Generic_plugins() const override
+  {
+    return QStringList() << "CopyGraphPlugin";
+  }
+
+
+  bool Generic_plugin(const QString &plugin) override
+  {
+    SimpleWindow *sw = static_cast< SimpleWindow * >(window);
+      // dynamic_cast fails under OS X
+    sw->onModificationParam("copy_graph_qt_p", this);
+      //TODO-elo-refactor-plugins
+      // 1) the name of the function onModificationParam()
+      //    is unrelated to its content!!!
+      // 2) Generic_plugin() is called by SimpleWindow
+      //    and calls back SimpleWindow functions here ;
+      //    looks like a bad architecture
+    sw->onApplyButton();
+
+    return true;
+  }
+
+signals:
+  void resetSignal();
+
+protected:
+  // filter output
+  void *m_output_mesh_void;
+  FEVV::PMapsContainer *m_output_pmaps_bag;
+};
+
+} // namespace FEVV
+
