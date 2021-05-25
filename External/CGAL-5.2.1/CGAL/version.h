e08a2df52565c6e02677baf2ae1ce7e5c68a1f04
elombardi2 <eric.lombardi@liris.cnrs.fr>
2021-05-25 15:32:59 +0200

    Extract CGAL 5.2.1 headers into External

---
+++ External/CGAL-5.2.1/CGAL/version.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2009
+// Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland),
+// INRIA Sophia-Antipolis (France),
+// Max-Planck-Institute Saarbruecken (Germany),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org)
+//
+// $URL: https://github.com/CGAL/cgal/blob/v5.2.1/Installation/include/CGAL/version.h $
+// $Id: version.h 3c90fd8 2020-12-23T16:54:48+01:00 Laurent Rineau
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : -
+
+#ifndef CGAL_VERSION_H
+#define CGAL_VERSION_H
+
+#ifndef SWIG
+#define CGAL_VERSION 5.2.1
+#define CGAL_GIT_HASH c733c2a9874a9b47a03cbf2bfade32c0a2b51fee
+#endif
+#define CGAL_VERSION_NR 1050211000
+#define CGAL_SVN_REVISION 99999
+#define CGAL_RELEASE_DATE 20210313
+
+#include <CGAL/version_macros.h>
+
+#endif
