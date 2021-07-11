b7a07fe65c95e56fa98d9fb381044272a0494ad8
Martial TOLA <martial.tola@liris.cnrs.fr>
2021-07-11 12:42:27 +0200

    update mepp version and changelog

--- Visualization/Applications/mepp-gui.h
+++ Visualization/Applications/mepp-gui.h
@@ -11,7 +11,7 @@
 #pragma once
 
 // -----------------------------------------------------------------------
-#define MEPP_VERSION "v0.14.2 - 16/02/2021"
+#define MEPP_VERSION "v0.14.3 - 11/07/2021"
 
 #define MAINWINDOW_TITLE QObject::tr("MEPP2 : 3D MEsh Processing Platform")
 
