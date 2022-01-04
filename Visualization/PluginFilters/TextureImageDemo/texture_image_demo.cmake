5587a3088daf4512f868f76b0ae03756e9595b32
Eric Lombardi <eric.lombardi@liris.cnrs.fr>
2022-01-04 16:43:10 +0100

    Add a plugin for Texture Image Demo filter

---
+++ Visualization/PluginFilters/TextureImageDemo/texture_image_demo.cmake
@@ -0,0 +1,38 @@
+# --> TextureImageDemoPlugin : QtPlugin [BEGIN]
+OPTION( BUILD_USE_GUI_TextureImageDemoPlugin "BUILD TextureImageDemoPlugin " ON )
+if (BUILD_USE_GUI_TextureImageDemoPlugin)
+  set( TextureImageDemo_Qt_Plugin_HEADER
+       "${CMAKE_CURRENT_LIST_DIR}/texture_image_demo_plugin.h"
+       "${CMAKE_CURRENT_LIST_DIR}/Dialogs/texture_image_demo_dialog.h"
+       )
+  set( TextureImageDemo_Qt_Plugin_UI
+       "${CMAKE_CURRENT_LIST_DIR}/Dialogs/texture_image_demo_dialog.ui"
+       )
+  if (BUILD_USE_QT5)
+    QT5_WRAP_CPP(TextureImageDemo_Qt_Plugin_MOC_CPP ${TextureImageDemo_Qt_Plugin_HEADER})
+    QT5_WRAP_UI(TextureImageDemo_Qt_Plugin_UI_CPP ${TextureImageDemo_Qt_Plugin_UI})
+    set(TextureImageDemo_Qt_Plugin_SRC ${TextureImageDemo_Qt_Plugin_SRC} ${TextureImageDemo_Qt_Plugin_MOC_CPP} ${TextureImageDemo_Qt_Plugin_UI_CPP})
+  else(BUILD_USE_QT5)
+    QT4_WRAP_CPP(TextureImageDemo_Qt_Plugin_MOC_CPP ${TextureImageDemo_Qt_Plugin_HEADER})
+    QT4_WRAP_UI(TextureImageDemo_Qt_Plugin_UI_CPP ${TextureImageDemo_Qt_Plugin_UI})
+    set(TextureImageDemo_Qt_Plugin_SRC ${TextureImageDemo_Qt_Plugin_SRC} ${TextureImageDemo_Qt_Plugin_MOC_CPP} ${TextureImageDemo_Qt_Plugin_UI_CPP})
+  endif(BUILD_USE_QT5)
+
+  add_library(
+      TextureImageDemoPlugin
+      SHARED
+      "${CMAKE_CURRENT_LIST_DIR}/texture_image_demo_plugin.cpp"
+      "${CMAKE_CURRENT_LIST_DIR}/Dialogs/texture_image_demo_dialog.cpp"
+      ${TextureImageDemo_Qt_Plugin_SRC}
+      ${osgQt_SRC} # from viewer
+      )
+  target_link_libraries (TextureImageDemoPlugin ${TextureImageDemo_Qt_Plugin_LIB}
+    ${GUILIB_DEMO} # from viewer
+    )
+  add_dependencies(TextureImageDemoPlugin mepp-gui)
+endif (BUILD_USE_GUI_TextureImageDemoPlugin)
+# --> TextureImageDemoPlugin : QtPlugin [END]
+
+# remove the lines below to integrate into MEPP2-public
+#TODO-elo why is this line needed ?
+include_directories( "${CMAKE_CURRENT_BINARY_DIR}" )
---
+++ Visualization/PluginFilters/TextureImageDemo/texture_image_demo_plugin.h
@@ -0,0 +1,226 @@
+// Copyright (c) 2012-2019 University of Lyon and CNRS (France).
+// All rights reserved.
+//
+// This file is part of MEPP2; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation; either version 3 of the License, 
+// or (at your option) any later version.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#pragma once
+
+#if(_MSC_VER >= 1400)
+#ifndef _SCL_SECURE_NO_WARNINGS
+#define _SCL_SECURE_NO_WARNINGS
+#endif
+#endif
+
+#include "Visualization/Plugins/PluginInterface.h"
+
+#include <QStringList>
+#include "Dialogs/texture_image_demo_dialog.h"
+
+#ifndef Q_MOC_RUN // MT : very important to avoid the error : ' Parse error at
+                  // "BOOST_JOIN" ' -> (qt4 pb with boost)
+#include "Visualization/PluginFilters/BasePluginQt.h"
+#include "Visualization/SimpleViewer.h"
+
+#include "Visualization/SimpleWindow.h"
+
+// A) include the header of the filter corresponding to your operation
+#include "Examples/Generic/TextureImageDemo/texture_image_demo_filter.hpp"
+
+#include "FEVV/Wrappings/properties.h"
+
+#ifdef FEVV_USE_CGAL
+#include "FEVV/Wrappings/properties_polyhedron_3.h"
+#include "FEVV/Wrappings/properties_surface_mesh.h"
+#include "FEVV/Wrappings/properties_linear_cell_complex.h"
+#endif // FEVV_USE_CGAL
+#ifdef FEVV_USE_OPENMESH
+#include "FEVV/Wrappings/properties_openmesh.h"
+#endif // FEVV_USE_OPENMESH
+#ifdef FEVV_USE_AIF
+#include "FEVV/Wrappings/properties_aif.h"
+#endif // FEVV_USE_AIF
+#endif
+
+namespace FEVV {
+
+class TextureImageDemoPlugin : public QObject,
+                         public Generic_PluginInterface,
+                         public BasePluginQt
+{
+  Q_OBJECT
+  Q_INTERFACES(FEVV::Generic_PluginInterface)
+#if(FEVV_USE_QT5) // see at the end of .cpp for QT4
+  Q_PLUGIN_METADATA(IID "TextureImageDemoPlugin")
+#endif
+
+  /*public:
+      using BasePlugin::apply;*/
+public:
+  TextureImageDemoPlugin() = default;
+  ~TextureImageDemoPlugin() = default;
+
+public:
+  void init() override { init(1.0, 1.0, 1.0); }
+
+  void init(double /*_x*/, double /*_y*/, double /*_z*/)
+  {
+    /*
+    value_x = _x;
+    value_y = _y;
+    value_z = _z;
+    */
+  }
+
+  void reset() override
+  {
+    init();
+
+    emit resetSignal();
+  }
+
+  void addParameters(BaseWindow *_window) override
+  {
+    window = _window;
+    if(window == nullptr || !window->isInit())
+    {
+      std::cerr << "BaseWindow is null or not initialized." << std::endl;
+      return;
+    }
+  }
+
+  template< typename HalfedgeGraph >
+  void process(HalfedgeGraph *_mesh, FEVV::PMapsContainer *pmaps_bag)
+  {
+    std::cout << "Asking to TextureImageDemo mesh ! " << std::endl;
+
+    // retrieve the materials property map
+
+    if(! has_map(*pmaps_bag, FEVV::mesh_materials))
+    {
+      std::cout << "The mesh has no materials. Aborting." << std::endl;
+      return;
+    }
+
+    std::cout << "retrieve existing mesh_materials property map" << std::endl;
+    auto mtl_pm = get_property_map(FEVV::mesh_materials, *_mesh, *pmaps_bag);
+
+    // apply filter
+    // B) call the filter corresponding to your operation
+    texture_image_demo_filter(*_mesh, mtl_pm);
+
+    std::cout << "TextureImageDemo successfully applied." << std::endl;
+  }
+
+
+  template< typename HalfedgeGraph >
+  void applyHG(BaseAdapterVisu *_adapter,
+               HalfedgeGraph *_mesh,
+               FEVV::PMapsContainer *pmaps_bag)
+  {
+    // get filter parameters from dialog window
+    TextureImageDemoDialog dialog;
+    dialog.setParameters(/*value_x, value_y, value_z*/);
+    if(dialog.exec() == QDialog::Accepted)
+      dialog.getParameters(/*value_x, value_y, value_z*/);
+    else
+      return; // abort applying filter
+
+    // apply filter
+    process(_mesh, pmaps_bag);
+
+    // redraw mesh
+    SimpleViewer *viewer =
+        dynamic_cast< SimpleViewer * >(_adapter->getViewer());
+    if(viewer)
+      viewer->draw_or_redraw_mesh(_mesh, pmaps_bag, true, false);
+
+    // comment next line to keep parameters values between calls
+    reset();
+
+    viewer->frame();
+  }
+
+
+#ifdef FEVV_USE_OPENMESH
+  void apply(BaseAdapterVisu *_adapter,
+             MeshOpenMesh *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    applyHG< MeshOpenMesh >(_adapter, _mesh, pmaps_bag);
+  }
+#endif
+
+#ifdef FEVV_USE_CGAL
+  void apply(BaseAdapterVisu *_adapter,
+             MeshLCC *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    applyHG< MeshLCC >(_adapter, _mesh, pmaps_bag);
+  }
+
+  void apply(BaseAdapterVisu *_adapter,
+             MeshSurface *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    applyHG< MeshSurface >(_adapter, _mesh, pmaps_bag);
+  }
+
+  void apply(BaseAdapterVisu *_adapter,
+             MeshPolyhedron *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    applyHG< MeshPolyhedron >(_adapter, _mesh, pmaps_bag);
+  }
+#endif
+
+#ifdef FEVV_USE_AIF
+  void apply(BaseAdapterVisu *_adapter,
+             MeshAIF *_mesh,
+             FEVV::PMapsContainer *pmaps_bag) override
+  {
+    applyHG< MeshAIF >(_adapter, _mesh, pmaps_bag);
+  }
+#endif
+
+
+  QStringList Generic_plugins() const override
+  {
+    return QStringList() << "TextureImageDemoPlugin";
+  }
+
+
+  bool Generic_plugin(const QString &/*plugin*/) override
+  {
+    SimpleWindow *sw = static_cast< SimpleWindow * >(window);
+      // dynamic_cast fails under OS X
+    sw->onModificationParam("texture_image_demo_qt_p", this);
+      //TODO-elo-refactor-plugins
+      // 1) the name of the function onModificationParam()
+      //    is unrelated to its content!!!
+      // 2) Generic_plugin() is called by SimpleWindow
+      //    and calls back SimpleWindow functions here ;
+      //    looks like a bad architecture
+    sw->onApplyButton();
+
+    return true;
+  }
+
+signals:
+  void resetSignal();
+
+protected:
+  // filter parameters
+  /*
+  double value_x = 0;
+  double value_y = 0;
+  double value_z = 0;
+  */
+};
+
+} // namespace FEVV
+
