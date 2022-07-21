2be00641a4698a593ed1ab0f0da62145368a969b
Vincent Vidal <vincent.vidal@liris.cnrs.fr>
2022-07-21 17:34:35 +0200

    added copyrights for progressive_compression_filter_cgal_surface_mesh.cpp file

--- Examples/CGAL/Surface_mesh/progressive_compression_filter_cgal_surface_mesh.cpp
+++ Examples/CGAL/Surface_mesh/progressive_compression_filter_cgal_surface_mesh.cpp
@@ -1,5 +1,15 @@
-// The following include defines the FEVV::MeshSurface type
+// Copyright (c) 2012-2022 University of Lyon and CNRS (France).
+// All rights reserved.
+//
+// This file is part of MEPP2; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of
+// the License, or (at your option) any later version.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
+// The following include defines the FEVV::MeshSurface type
 #include "FEVV/DataStructures/DataStructures_cgal_surface_mesh.h"
 
 #include "FEVV/Wrappings/Geometry_traits_cgal_surface_mesh.h"
